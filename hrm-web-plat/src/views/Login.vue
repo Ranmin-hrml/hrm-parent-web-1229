c02fd96a742fa6d9b5a0efa7ac6a392c65a144da
ranmin <1606905432@qq.com>
2020-06-19 20:21:14 +0800

    前台代码提交

--- hrm-web-plat/src/views/Login.vue
+++ hrm-web-plat/src/views/Login.vue
@@ -12,80 +12,87 @@
       <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
       <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
+    <el-form-item style="width:100%;">
+      <el-button type="default" style="width:100%;" @click.native.prevent="handleRegister" >租户入驻</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
   </el-form>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        logining: false,
-        ruleForm2: {
-          account: 'admin',
-          checkPass: '123456'
+    export default {
+        data() {
+            return {
+                logining: false,
+                ruleForm2: {
+                    account: 'admin',
+                    checkPass: '123456'
+                },
+                rules2: {
+                    account: [
+                        { required: true, message: '请输入账号', trigger: 'blur' },
+                        //{ validator: validaePass }
+                    ],
+                    checkPass: [
+                        { required: true, message: '请输入密码', trigger: 'blur' },
+                        //{ validator: validaePass2 }
+                    ]
+                },
+                checked: true
+            };
         },
-        rules2: {
-          account: [
-            { required: true, message: '请输入账号', trigger: 'blur' },
-            //{ validator: validaePass }
-          ],
-          checkPass: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
-            //{ validator: validaePass2 }
-          ]
-        },
-        checked: true
-      };
-    },
-    methods: {
-      handleReset2() {
-        this.$refs.ruleForm2.resetFields();
-      },
-      handleSubmit2(ev) {
-        var _this = this;
-        this.$refs.ruleForm2.validate((valid) => {
-          if (valid) {
-            //_this.$router.replace('/table');
-            this.logining = true;
-            //NProgress.start();
-            var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
-
-            this.$http.post("/system/login",loginParams)
-                .then(res=>{
-                    let {success,message,resultObj} = res.data;
-                    if (!success) {
-                        this.$message({
-                            message: message,
-                            type: 'error'
-                        });
+        methods: {
+            //跳转到租户注册
+            handleRegister(){
+                this.$router.push({ path: '/register' });
+            },
+            handleReset2() {
+                this.$refs.ruleForm2.resetFields();
+            },
+            handleSubmit2(ev) {
+                var _this = this;
+                this.$refs.ruleForm2.validate((valid) => {
+                    if (valid) {
+                        //_this.$router.replace('/table');
+                        this.logining = true;
+                        //NProgress.start();
+                        var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
+                        this.$http.post("/system/login",loginParams)
+                            .then(res=>{
+                            let {success,message,resultObj} = res.data;
+                        if (!success) {
+                            this.$message({
+                                message: message,
+                                type: 'error'
+                            });
+                        } else {
+                            sessionStorage.setItem('user', JSON.stringify(resultObj));
+                            this.$router.push({ path: '/table' });
+                        }
+                    })
+                        /*requestLogin(loginParams).then(data => {
+                          this.logining = false;
+                          //NProgress.done();
+                          let { msg, code, user } = data;
+                          if (code !== 200) {
+                            this.$message({
+                              message: msg,
+                              type: 'error'
+                            });
+                          } else {
+                            sessionStorage.setItem('user', JSON.stringify(user));
+                            this.$router.push({ path: '/table' });
+                          }
+                        });*/
                     } else {
-                        sessionStorage.setItem('user', JSON.stringify(resultObj));
-                        this.$router.push({ path: '/table' });
-                    }
-                })
-
-            /*requestLogin(loginParams).then(data => {
-              this.logining = false;
-              //NProgress.done();
-              let { msg, code, user } = data;
-              if (code !== 200) {
-                this.$message({
-                  message: msg,
-                  type: 'error'
-                });
-              } else {
-                sessionStorage.setItem('user', JSON.stringify(user));
-                this.$router.push({ path: '/table' });
-              }
-            });*/
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      }
+                        console.log('error submit!!');
+                return false;
+            }
+            });
+            }
+        }
     }
-  }
+
 
 </script>
 
